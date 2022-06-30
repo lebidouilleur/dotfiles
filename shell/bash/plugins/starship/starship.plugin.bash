c306b0f5984b4c6352a431e4054a68f6997d1c10
lebidouilleur <lebidouilleur@icloud.com>
2022-06-30 19:05:20 +0200

    Allow starship as plugin

---
+++ shell/bash/plugins/starship/starship.plugin.bash
@@ -0,0 +1,16 @@
+### starship.plugin.bash ---                                    -*- mode: bash; -*-
+
+## Copyright (C) 2022  lebidouilleur
+
+## Author: lebidouilleur <lebidouilleur@icloud.com>
+## Keywords:
+
+### Commentary:
+
+##
+
+### Code:
+
+
+
+eval "$(starship init bash)"
